049f70f51d2cff7790c0970155e4281cd4877b9d
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-03-07 14:45:00 +0530

    update CrudDAO interface

--- src/main/java/lk/ijse/zanystore/dao/CrudDAO.java
+++ src/main/java/lk/ijse/zanystore/dao/CrudDAO.java
@@ -1,4 +1,25 @@
-package lk.ijse.zanystore.dao.custom;
+package lk.ijse.zanystore.dao;
 
-public interface CrudDAO {
+import lk.ijse.zanystore.dao.SuperDAO;
+import lk.ijse.zanystore.dto.EmployeeDTO;
+import lk.ijse.zanystore.entity.Customer;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudDAO<T> extends SuperDAO {
+
+    public boolean save(T entity) throws SQLException;
+
+    public boolean update(T entity) throws SQLException;
+
+    public boolean delete(int id) throws SQLException;
+
+    public List<T> getAll() throws SQLException;
+
+    public String showNextId() throws SQLException;
+
+    public T find(int empId) throws SQLException;
+
+    public String getId() throws SQLException;
 }
