320c8ab9738df512998c3a62a4d2df0ed7d66a32
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-03-08 16:05:29 +0530

    implement BOFactory class

--- src/main/java/lk/ijse/zanystore/bo/BOFactory.java
+++ src/main/java/lk/ijse/zanystore/bo/BOFactory.java
@@ -1,25 +1,44 @@
-//package lk.ijse.zanystore.bo;
-//
-//public class BOFactory {
-//    private static BOFactory instance;
-//    private BOFactory(){}
-//    public static BOFactory getInstance() {
-//        return instance==null?instance=new BOFactory():instance;
-//    }
-//    public enum BOTypes{
-//        CUSTOMER,ITEM,PLACE_ORDER
-//    }
-//    public SuperBO getBOFactory(BOTypes boType){
-//        switch (boType){
-//            case CUSTOMER:
-//                return new CustomerBOImpl();
-//            case ITEM:
-//                return new ItemBOImpl();
-//            case PLACE_ORDER:
-//                return new PlaceOrderBoImpl();
-//            default:
-//                return null;
-//        }
-//    }
-//
-//}
+package lk.ijse.zanystore.bo;
+
+import lk.ijse.zanystore.bo.custom.SuperBO;
+import lk.ijse.zanystore.bo.custom.impl.*;
+
+public class BOFactory {
+    private static BOFactory instance;
+    private BOFactory(){}
+    public static BOFactory getInstance() {
+        return instance==null?instance=new BOFactory():instance;
+    }
+    public enum BOTypes{
+        CUSTOMER,ITEM,PLACE_ORDER,QUOTATION,EMPLOYEE,LAYOUT,PAYMENT,RETURN,SERVICEPROVIDER,SUPPLIER,USER
+    }
+    public SuperBO getBOFactory(BOTypes boType){
+        switch (boType){
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case ITEM:
+                return new ItemBOImpl();
+            case PLACE_ORDER:
+                return new PlaceOrderBOImpl();
+            case QUOTATION:
+                return new CreateQuotationBOImpl();
+            case EMPLOYEE:
+                return new EmployeeBOImpl();
+            case LAYOUT:
+                return new LayoutBOImpl();
+            case PAYMENT:
+                return new PaymentBOImpl();
+            case RETURN:
+                return new ReturnBOImpl();
+            case SERVICEPROVIDER:
+                return new ServiceProviderBOImpl();
+            case SUPPLIER:
+                return new SupplierBOImpl();
+            case USER:
+                return new UserBOImpl();
+            default:
+                return null;
+        }
+    }
+
+}
